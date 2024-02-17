b87d06afb8d59b7bad09a53563a6db7a214ddb72
Marshal10 <marshalmaleane123@gmail.com>
2024-02-17 15:36:51 +0530

    Check if the queue is full

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ class Queue:
     def __str__(self):
         values=[str(x) for x in self.items]
         return ' '.join(values)
+    
+    def isFull(self):
+        if self.top+1==self.start:
+            return True
+        elif self.start==0 and self.top+1==self.maxSize:
+            return True
+        return False
+    
         
 customQueue=Queue(3)
-print(customQueue)
+print(customQueue.isFull())
